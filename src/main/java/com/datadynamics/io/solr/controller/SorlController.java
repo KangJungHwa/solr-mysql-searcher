27d4c33a02455299ee6617c088b8ec63f019c9ab
pacino <pacino.kang@gmail.com>
2021-07-14 20:18:21 +0900

    findByjobname

--- src/main/java/com/datadynamics/io/solr/controller/SorlController.java
+++ src/main/java/com/datadynamics/io/solr/controller/SorlController.java
@@ -8,6 +8,8 @@ import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -47,25 +50,33 @@ public class SorlController {
 
         return ResponseEntity.ok(MapUtils.map("elapsed (ms)", endMillis, "name", optionalJob.get().getJobName()));
     }
-    @GetMapping("/search/product")
+    @GetMapping("/search/cat_std_word")
     public ResponseEntity findProduct(@RequestParam  String searchStr) {
         //http://localhost:9988/service/solr/search/product?searchStr=The
-        log.info("{}", String.format("Product을 '%s'로 검색합니다.", searchStr));
+        log.info("{}", String.format("테이블을 '%s'로 검색합니다.", searchStr));
         long startMillis = System.currentTimeMillis();
-        HttpSolrClient httpSolrClient = new HttpSolrClient.Builder("http://localhost:8983/solr/techproducts").build();
+        HttpSolrClient httpSolrClient = new HttpSolrClient.Builder("http://localhost:8983/solr/cat_std_word").build();
         SolrQuery solrQuery = new SolrQuery();
-        String query=String.format("name:'%s'", searchStr);
+        //String query=String.format("name:'%s'", searchStr);
+        String query=String.format("*:*");
         solrQuery.setQuery(query);
-        solrQuery.setHighlight(true);
-        solrQuery.set("hl.fl","name");
-
-        solrQuery.set("fl","*");
-        solrQuery.setRows(10);
-        QueryResponse solrResponse=null;
+        QueryResponse queryResponse=null;
         try {
-            solrResponse = httpSolrClient.query(solrQuery);
-            System.out.println(solrResponse);
-            System.out.println("Total Documents : "+solrResponse.getResults().getNumFound());
+          queryResponse = httpSolrClient.query(solrQuery);
+          SolrDocumentList solrDocs = queryResponse.getResults();
+          Iterator<SolrDocument> iterator = solrDocs.iterator();
+        while (iterator.hasNext()) {
+            SolrDocument solrDocument = iterator.next();
+//          String docId = (String) solrDocument.getFieldValue("id");
+            System.out.println("solrDocument " + solrDocument);
+            String synonym = (String) solrDocument.getFieldValue("synonym");
+            String[] arrSyn=synonym.split(",");
+            for ( String word:arrSyn) {
+                System.out.println("word " + word.toString());
+            }
+            System.out.println("Document " + synonym);
+        }
+
         } catch (SolrServerException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -73,13 +84,9 @@ public class SorlController {
         }
 
 
-//        Optional<Job> optionalJob = solrService.findByJobName(jobName);
-//        System.out.println(optionalJob.get());
-
         String endMillis = String.valueOf(System.currentTimeMillis() - startMillis);
         log.info("총 처리 시간은 {}ms 입니다.", endMillis);
-        //ResponseEntity responseEntity = new ResponseEntity();
 
-        return ResponseEntity.ok(MapUtils.map("elapsed (ms)", endMillis, "name", solrResponse.getResults().getNumFound()));
+        return ResponseEntity.ok(MapUtils.map("elapsed (ms)", endMillis, "name", queryResponse.getResults().getNumFound()));
     }
 }
